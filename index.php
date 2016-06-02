006703f78d547ea2a418256912d7e7158d421498
redrails <redrailz@gmail.com>
2016-06-03 00:12:36 +0100

    website update

---
+++ index.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>~ IHTASHAM </title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="google-site-verification" content="s5U7V7CATch7C5ZR4gvglFD65_0CKdGXFuix0JhZeHs" />
+        <meta name="robots" content="noarchive" />
+
+        <meta name="description" content="I am a front-end and PHP web developer" />
+
+        <link href="http://fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet" type="text/css" />
+
+
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/favicon-144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicon-152.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicon-120.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180.png"/>
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <link href="css/main.css" rel="stylesheet" />
+    </head>
+
+    <body>
+
+        <div id="page-container">
+            <div id="page-wrapper">
+                <header id="masthead" class="clearfix">
+                    <a id="masthead-name" href="/">IHTASHAM</a>
+
+                    <nav id="main-nav">
+                        <ul>
+                            <li>
+                                <a href="index.php"  class="active">ABOUT</a>
+                            </li>
+                            <li>
+                                <a href="projects.html">PROJECTS</a>
+                            </li>
+                            <li>
+                                <a href="http://blog.ihtasham.com" >BLOG</a>
+                            </li>
+			    <li>
+				<a href="paste"> PASTE </a>
+			    </li>
+
+
+                            <li class="final">
+                                <a href="contact.html" >Contact</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                <main>
+<br>                    
+
+<div id="portfolio">
+    <h1>~ WHOAMI</h1>
+
+    <section class="github-callout">
+        <svg class="mail icon"><use xlink:href="#icon-github"></use></svg>
+        If you're just interested in code, check out
+        <a href="https://github.com/redrails" rel="external" target="_blank">my GitHub</a>
+    </section>
+
+    <section>
+        <h2 class="subhead">ME</h2>
+        <p>
+		I'm known as Toby, ihtasham and also redrails.
+		With more than 5 years of experience in programming and development I'm studying computer science at university.
+		I also love HD things that look good on my screen. I also love Python and C++. 
+        </p>
+    </section>
+    <section>
+        <h2 class="subhead">KNOWLEDGE</h2>
+        <p>
+		I consider myself to be proficient in PHP, JavaScript, C++, Python and Java. I am currently learning to use my Java skils to develop
+		Android applications and I consider myself to be the biggest Apple hater on the planet <i>:)</i>. I have used many framworks to develop
+		applications including SpringMVC, Laravel, Boost and Flask.
+        </p>
+
+    </section>
+    <section>
+
+        <h2 class="subhead">SOME OTHER STUFFS</h2>
+<br>
+        <p>
+		<b>REVERSEHOSTS</b>: In September 2012 I founded a company with a friend of mine called ReverseHosts. It was pretty cool
+		and I got to learn a lot about networking, marketing and I was the systems administrator for the VPS's. Cloud computing is marvellous.
+		</p>
+
+        <p>
+         <b>365DaysOfIhtasham</b>: I started a 365 days project in 2014 which ended in the beginning 2015. In this project my aim was to take a picture of myself or something 
+		 fascinating that happened during the day, everyday (hence 365) and caption it with something about myself, my day or my opinion/understandings on something. 
+		 So far the project has been very successful with over 100 followers and supporters. 
+        <p>
+
+    </section>
+
+   
+
+</div>
+
+                </main>
+
+            </div>
+        </div>
+        <footer id="site-footer">
+            <p>
+                <i> ~ insert copyright info here so website looks cool </i>
+            </p>
+        </footer>
+
+
+        <a href="/ignis-fatuus" class="hidden"></a>
+
+    </body>
+</html>
